1cf449ce4ef3da58f30a8de69046a37b24723c85
I <henrikl@tuta.io>
2022-04-11 12:15:08 +0200

    bugfix

--- youtube-setup.sh
+++ youtube-setup.sh
@@ -3,6 +3,8 @@ BASEDIR=$(dirname "$0")
 
 if [ -e $BASEDIR/installed/youtube-setup ]; then
     if [[ "$(ping -c 1 8.8.8.8 | grep '100% packet loss' )" != "" ]]; then
+        echo "No Internet"
+    else
         cp -r /Users/i/Creative\ Cloud\ Files/_video /Users/i/Creative\ Cloud\ Files/new-vid
         cd /Users/i/Creative\ Cloud\ Files/new-vid/input
         gdown --folder --id 1Xex1jsN5DB8ShB4jUVTuqJVLSvjVGwJy
@@ -12,9 +14,9 @@ if [ -e $BASEDIR/installed/youtube-setup ]; then
         mv $VIDEO_NAME $STRIPPED_AUDIO_NAME.MOV 
         mv /Users/i/Creative\ Cloud\ Files/new-vid /Users/i/Creative\ Cloud\ Files/$STRIPPED_AUDIO_NAME
         open /Applications/Adobe\ Premiere\ Pro\ 2022/Adobe\ Premiere\ Pro\ 2022.app
+    fi
 else
     pip install gdown
     echo 'alias yt="/Users/i/Documents/github/scripts/youtube-setup.sh"' >> ~/.zshrc
     touch $BASEDIR/installed/youtube-setup
 fi
-
